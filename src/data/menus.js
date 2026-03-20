828bb08e7a490fafd615d8a1de2cd1783be75302
fluidlog <yannickduthe@gmail.com>
2026-03-20 23:26:10 +0100

    test

--- src/data/menus.js
+++ src/data/menus.js
@@ -1,21 +1,11 @@
 export const menuMain = [
     {
-        name: "Home",
+        name: "Accueil",
         url: "/",
         highlighted: false,
     },
     {
-        name: "Menu",
-        url: "/menu",
-        highlighted: false,
-    },
-    {
-        name: "About Us",
-        url: "/about-us",
-        highlighted: false,
-    },
-    {
-        name: "Gallery",
+        name: "Gallerie",
         url: "/gallery",
         highlighted: false,
     },
@@ -23,38 +13,5 @@ export const menuMain = [
         name: "Contact",
         url: "/contact",
         highlighted: false,
-    },
-    {
-        name: "Book",
-        url: "/book",
-        highlighted: true,
-    },
-];
-
-export const footerPrimary = [
-    {
-        title: "Navigatie",
-        items: [
-            { name: "home", url: "/" },
-        ],
-    },
-];
-
-export const footerSecondary = [
-    {
-        title: "support",
-        items: [
-            { name: "pricing", url: "/pricing" },
-        ],
-    },
-];
-
-export const footerTertiary = [
-    {
-        title: "Links",
-        items: [
-            { name: "privacybeleid", url: '/privacy-policy' },
-            { name: "api status", url: "/api-status" },
-        ],
     },
 ];
